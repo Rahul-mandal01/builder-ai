ef404634d46f78289b3c143bba7a68dd3a567970
Rahul-mandal01 <rahulra16809999@gmail.com>
2026-09-10 19:53:59 +0530

    Add HomePage component with user context and layout structure

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import { useAppContext } from '../context/AppContext';
 
 const HomePage = () => {
+  const { user } = useAppContext();
+
   return (
-    <div>HomePage</div>
+    <div className="h-screen overflow-y-scroll text-white font-sans 
+    bg-[url('/bg-img.png')] bg-cover bg-center bg-no-repeat ">
+      {/* Navbar */}
+      <nav className="sticky top-0 z-10 flex items-center justify-between
+        px-6 py-4">
+        <div className="flex items-center gap-2">
+          <img src="/logo.svg" alt="logo" className="size-6" />
+          <span className="text-xl font-semibold tracking-tight">BuilderAI</span>
+        </div>
+
+        <div className="flex items-center gap-4 text-sm font-medium 
+        text-zinc-300">
+          <span>{user?.name}</span>
+          <button className="py-1.5 px-3 border border-white/20 text-white hover:bg-white/10
+          text-sm rounded-md cursor-pointer bg-transparent">
+            Sign out
+          </button>
+        </div> 
+      </nav>
+
+      {/* Hero Section */}
+      <div className="flex-1 flex flex-col items-center justify-center px-6 pb-20 mt-8
+      xl:mt-28">
+
+        <div className="flex flex-col items-center max-w-2xl w-full">
+          {/* Promo badge */}
+          <div className="flex items-center gap-2 p-1.5 pr-3 bg-white/10
+          backdrop-blur-md rounded-full border border-white/20 text-[13px]
+          text-white/90">
+            <span className="px-3 py-1 text-[11px] bg-red-700 rounded-full
+            font-medium tracking-wider">PROMO</span>
+            <span>Create your first project for free</span>
+          </div>
+          
+          {/* Title */}
+          <h1 className="text-center text-4xl md:text-6xl font-medium 
+          mt-4 max-w-2xl text-white">
+            Let's build your app together
+          </h1>
+          <p className="text-center text-sm md:text-base max-w-xl mt-4 text-white/65
+          leading-relaxed">
+            Describe your Idea and watch AI design, structure and launch your website
+            instantly. No coding required.
+          </p>
+
+          {/* Prompt input with glassmorphic variant */}
+          <div className="w-full mt-6">
+              
+          </div>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
 
